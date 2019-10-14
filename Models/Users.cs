72c54bfafe1d9f15588f4c8bce8adfa57e7e3b67
Geiler97 <geilerroblero@gmail.com>
2019-10-14 16:48:07 -0500

    commit geiler

---
+++ Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace crudmvc
+{
+    public partial class Users
+    {
+        public int Iduser { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/UsersRoles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace crudmvc
+{
+    public partial class UsersRoles
+    {
+        public int IduserUr { get; set; }
+        public string IdroleUr { get; set; }
+    }
+}
